e5d108cdacd049b7e5d2571292a2242105db3856
nao taka <naopontan@gmail.com>
2013-03-25 16:29:56 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nttwrss::Application.config.secret_token = '7c0594f383c1f6aab657455d11e58e978a4bf0056761d3663ea5637bc14125efb242a7a980a27a5135a25ba6bae3c3faa3cb200376bb8c0ddef67c08565f4418'
